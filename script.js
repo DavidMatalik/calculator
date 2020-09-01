0cf58f70d01e17c21bc339409768b30f33faee98
david <david.matalik@posteo.de>
2020-09-01 20:42:00 +0200

    Create Calculator structure and give it a basic styling

--- script.js
+++ script.js
@@ -13,3 +13,11 @@ function multiply (number1, number2) {
 function divide (number1, number2) {
     return number1 / number2; 
 }
+
+function operate (operator, number1, number2) {
+    return (operator === "+") ? add(number1, number2) :
+    (operator === "-") ? subtract(number1, number2) :
+    (operator === "*") ? multiply(number1, number2) :
+    (operator === "/") ? divide(number1, number2) :
+    console.log("Something went wrong");
+}
